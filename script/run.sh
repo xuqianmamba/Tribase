97b2ef410a8e5706369aba121b5f5cb9d088d242
panjd123 <1747366367@qq.com>
2024-05-08 08:59:35 -0400

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,10 +1,3 @@
 ./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --run_faiss --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
 
-./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratio 1.0 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
-./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratio 0.95 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
-./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratio 0.9 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
-./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratio 0.85 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
-./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratio 0.8 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
-./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratio 0.75 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
-./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratio 0.7 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
-./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratio 0.65 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
+./release/bin/query --benchmarks_path ./benchmarks --dataset sift1m --high_precision_subNN_index --opt_levels 1 --ratios 1.0 0.95 0.9 0.85 0.8 0.75 0.7 0.65 --loop 3 --nprobes 1 3 5 7 10 30 50 70 100 200 300 500 # 700 0
