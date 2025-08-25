b46c1f7bb4471fc087d230b39a34203608290579
panjd123 <xm_jarden@qq.com>
2025-08-26 04:00:06 +0800

    feat(figures): add build_ratio2 script and plot fig12b2 with updated run.sh

---
+++ figures/build_ratio2.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+target="build_ratio2"
+
+loop=10
+iloop=10
+dataset=fasion_mnist_784
+subk=1000
+force=0
+
+PARSED_ARGS=$(getopt -o l:d:s:k:f:i: --long loop:,iloop:,dataset:,subk:,force: -n "$0" -- "$@")
+eval set -- "$PARSED_ARGS"
+while true; do
+    case "$1" in
+        -l|--loop)
+            loop="$2"
+            shift 2 # 移过选项名和它的值
+            ;;
+        -i|--iloop)
+            iloop="$2"
+            shift 2 # 移过选项名和它的值
+            ;;
+        -d|--dataset)
+            dataset="$2"
+            shift 2 # 移过选项名和它的值
+            ;;
+        -s|-k|--subk)
+            subk="$2"
+            shift 2 # 移过选项名和它的值
+            ;;
+        -f|--force)
+            force=1
+            shift 2 # 移过选项名
+            ;;
+        --) # '--' 是 getopt 添加的标记, 表示选项处理结束
+            shift
+            break
+            ;;
+        *) # 此处不应被执行
+            echo "内部解析错误!"
+            exit 1
+            ;;
+    esac
+done
+
+output_csv_file="./logs/$target-recall-qps-tribase.csv"
+output_log_file="./logs/$target-recall-qps-tribase.log"
+faiss_output_csv_file="./logs/$target-recall-qps-faiss.csv"
+
+if [ $force -eq 1 ]; then
+    rm -rf $output_csv_file $output_log_file
+fi
+
+declare -A nprobes_dict
+
+nprobes_dict["nuswide"]="1 3 5 7 10 30 50 70 100 150 200 250 300 350 400 450 500 510 513 516 0" # 518 518
+nprobes_dict["fasion_mnist_784"]="1 3 5 7 10 20 30 40 50 60 70 80 90 150 200 0" # 100 244
+nprobes_dict["msong"]="1 3 5 7 10 30 50 70 100 150 200 250 300 350 600 800 0" # 400
+nprobes_dict["sift1m"]="1 3 5 7 10 30 50 70 100 150 200 250 300 400 600 800 0" # ?
+nprobes_dict["glove25"]="1 3 5 7 10 30 50 70 100 150 200 250 300 350 400 500 600 750 900 0" # ?
+nprobes_dict["HandOutlines"]="1 3 5 7 10 12 14 16 18 20 22 24 26 28 0" # ?
+nprobes_dict["StarLightCurves"]="1 3 5 7 10 12 14 16 18 0" # ?
+
+sub_nprobe_ratios="1 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1"
+
+for ((i=1; i<=$loop; i++)); do
+    for sub_nprobe_ratio in $sub_nprobe_ratios; do
+        ./release/bin/query --benchmarks_path ./benchmarks --dataset $dataset \
+            --nprobes 0 \
+            --sub_nprobe_ratio $sub_nprobe_ratio \
+            --opt_levels OPT_SUBNN_L2 \
+            --loop $iloop \
+            --csv $output_csv_file \
+            --subk $subk \
+            --verbose | tee -a $output_log_file
+    done
+done
---
+++ figures/fig12b2.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import re
+
+# 设置全局字体为Type 1字体
+mpl.rcParams['pdf.fonttype'] = 42  # 设置为Type 1字体
+mpl.rcParams['ps.fonttype'] = 42   # 设置为Type 1字体
+
+df = pd.read_csv('logs/build_ratio2-recall-qps-tribase.csv')
+with open("logs/build_ratio2-recall-qps-tribase.log") as f:
+    text = f.read()
+    add_elapseds = re.findall(r'^add elapsed: ([\d.]+)s', text, re.MULTILINE)
+    add_elapseds = np.array([float(x) for x in add_elapseds])
+
+query_time = df["query_time"].values
+
+query_time = query_time.reshape(-1, 10).mean(axis=0)[1::2]
+add_elapseds = add_elapseds.reshape(-1, 10).mean(axis=0)[1::2]
+
+
+# 设置全局字体大小
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams.update({'font.size': 22})
+
+# 定义数据点
+# build_speedup = [1, 1.06, 1.1, 1.16, 1.26, 1.36, 1.43, 1.63, 1.8, 1.96]
+# query_time = [100, 99.95, 99.92, 99.8, 99.6, 99.3, 99.1, 98.5, 97.8, 96]
+build_speedup = add_elapseds / add_elapseds[-1]
+query_speedup = query_time / query_time[-1] * 100
+coeffs = np.polyfit(build_speedup, query_speedup, 2)
+a, b, c = coeffs
+build_smooth = np.linspace(build_speedup.min(), build_speedup.max(), 10)
+query_smooth = a * build_smooth**2 + b * build_smooth + c
+
+# 创建图形
+plt.figure(figsize=(7, 4.5))
+plt.plot(build_smooth, query_smooth, marker='o', linestyle='-', color='blue', markersize=8)
+# plt.plot(build_speedup, query_speedup, marker='o', linestyle='-.', color='red', markersize=8)
+
+# 设置图形的标题和坐标轴标签
+plt.title('Query Speed vs Build Speedup', fontsize=24, fontweight='normal', color='black')
+plt.xlabel('Build Speedup', fontsize=24, fontweight='normal', color='black')
+plt.ylabel('Query Speed (%)', fontsize=24, fontweight='normal', color='black')
+
+# 设置x轴和y轴的刻度
+# plt.xticks([1, 1.2, 1.4, 1.6, 1.8, 2], fontsize=24, fontweight='normal', color='black')
+# plt.yticks([96, 97, 98, 99, 100], fontsize=24, fontweight='normal', color='black')
+
+# 设置y轴的显示范围和x轴的显示范围，留有一定空间
+# plt.ylim(95.9, 100.1)
+# plt.xlim(0.95, 2.1)
+
+# 设置坐标轴颜色和粗细
+plt.gca().spines['top'].set_color('black')
+plt.gca().spines['top'].set_linewidth(1.5)
+plt.gca().spines['right'].set_color('black')
+plt.gca().spines['right'].set_linewidth(1.5)
+plt.gca().spines['left'].set_color('black')
+plt.gca().spines['left'].set_linewidth(1.5)
+plt.gca().spines['bottom'].set_color('black')
+plt.gca().spines['bottom'].set_linewidth(1.5)
+
+# 设置坐标轴刻度线的颜色和粗细
+plt.gca().tick_params(colors='black', width=1.5)
+
+# 不显示网格
+plt.grid(False)
+
+# 调整布局
+plt.subplots_adjust(bottom=0.17,left=0.2)  # 整个图表向下移动
+
+
+plt.tight_layout()
+# 保存图形
+plt.savefig('figures/fig12b2.png', dpi=300)
